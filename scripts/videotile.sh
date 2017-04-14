5673b23344ce6fb8ae69157c46bf708e890de8d0
juri <juri27@gmail.com>
2017-04-14 09:09:36 +0200

    fix tiling

--- scripts/videotile.sh
+++ scripts/videotile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#script that opens a number of videos in mpv
+#script that opens a number of videos in mpv in i3 tiles
 dir=$1
 num=${2:-4}
 name=${3:-watch}
@@ -8,15 +8,25 @@ i3-msg workspace $name
 
 for (( c=0; c<$num; c++ ))
 do
-	i3-msg split h
 	half=$(expr $num / 2)
 	if [ $c = $half ]; then
-		i3-msg focus parent
-		i3-msg focus parent
+		#go to top level parent and split vertically
+		# works for 4-8 4:3 screens
+		for (( d=0;  d<$half; d++ ))
+		do
+			i3-msg focus parent
+		done
 		i3-msg split v		
 	fi
-
-	file=$(find "$dir" -type f ! -name '*.jpg' | shuf -n 1)
+	
+	#one random video from directory
+	
+	file=$(find "$dir" -type f ! -name '*.jpg' -and ! -name '*.nfo' -and ! -name '*.db' -and ! -name '*.xml' | shuf -n 1)
 	mpv "$file" &
         sleep 5	
+	if [ $c = $half ]; then
+		#split horizontally again after first vertically placed video
+		i3-msg split h		
+	fi
+
 done
